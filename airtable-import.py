9fdbaaa32870e7d11c5ff0ef5cc5092af153d05b
aeritter <aritter@tamu.edu>
2020-02-07 12:17:32 -0600

    Debug added, remove creation of edited.txt files
    Beginning of code to help with debugging added. Now edits the .txt contents in memory instead of creating a file on the disk (unless debug is enabled, in which case it is placed on the disk to help understand what the program is seeing).

--- airtable-import.py
+++ airtable-import.py
@@ -1,17 +1,20 @@
 """
-    Ideally, it should work with the files in memory without needing to create
-    an edited.txt file. Needs work.
+    Notes!
+    
 """
 
 
 import re, os.path, subprocess, time
 import json, requests
 
+debug = True
+sendairtabletestdata = False
+
 with open('C:\\airtabletest\\api_key.txt', 'r') as key:     # location of .txt file containing API token
     api_key = key.read()
 
 with open('C:\\airtabletest\\url.txt', 'r') as url:         #   location of .txt file containing URL for the table in Airtable 
-    url = str(url.read())                                   #   (found in api.airtable.com, not the same as the URL you see when browsing)
+    url = url.read()                                        #   (found in api.airtable.com, not the same as the URL you see when browsing)
 
 filefolder = 'C:\\airtabletest\\python-test\\'              # location of .pdf files
 pdftotextlocation = 'C:\\airtabletest\\pdftotext'           # location of pdftotext.exe (obtained from xpdfreader.com commandline tools)
@@ -25,45 +28,60 @@ def checkfornewfiles():
                 filepath = filefolder+str(x)[:-4]           # create string of filepath to .txt file
                 filetype = "None"
                 print(filepath)
-                time.sleep(5)
+                while os.path.exists(filepath+".txt") != True:
+                    time.sleep(.5)
+                    print("Waiting for file creation")
 
-                n = open(filepath+'.txt', 'r+')
-                line1 = str(n.readline())                   # first line of .txt file
-                n.readline()                                # second line of .txt file
-                line3 = str(n.readline())                   # third line of .txt file
-                if line1.startswith("MACK"):
-                    filetype = "Mack"
-                elif line3.startswith("PAGE 1"):
+                with open(filepath+'.txt', 'r+') as c:
+                    n = c.read()
+                line1 = n.split('\n', 1)[0]                 # first line of .txt file
+                line2 = n.split('\n', 2)[1]                 # second line of .txt file
+                if line1.startswith("Welcome to Volvo"):
                     filetype = "Volvo"
+                elif line2.startswith("GSO:"):
+                    filetype = "Mack"
                 else:
                     print("Unknown format")
                     filetype = "None"
-                    os.rename(filepath, filepath[:-4]+" unknown format.txt")  # rename file if not matched
+                    x = 20
+                    while x > 0:
+                        try:
+                            os.rename(filepath+".txt", filepath+" unknown format.txt")  # rename file if not matched
+                            break
+                        except PermissionError:
+                            time.sleep(3)
+                            print("Permission error. Trying {x} more time(s)") 
+                            x -= 1
 
                 if filetype != "None":
                     print(filetype)
-                    replacenewlines(n, filepath)
-                    dataimport(filepath+' edited.txt', filetype)
-                    os.remove(filepath+'.txt')
-                    os.remove(filepath+' edited.txt')                    
+                    n = n.replace('\n', ' ')                # replace \n in file for easier RegEx processing (simplification)
+                    if debug == True:
+                        writefile(n, filepath)            
+                    dataimport(n, filetype)
+                    os.remove(filepath+'.txt')               
 
-                n.close()
                 print(x)
  #   except:
  #       print("something went wrong")
 
 
-def replacenewlines(n, filepath):                           # changes file formatting for RegEx use
-    m = n.read().replace('\n', ' ')
-    a = open(filepath+' edited.txt', 'w')
-    a.write(m)
+def writefile(n, filepath):                                 # write file for debugging
+    a = open(filepath+' (debug).txt', 'w')
+    a.write(n)
     a.close()
 
 def dataimport(file, filetype):                             #   takes the file and processes it to take out the relevant information
     time.sleep(1)                                           #   according to which vendor it came from, then returns a list of dictionaries
-    print("Importing %s info", filetype)                    #   to be uploaded to Airtable
+    print(f"Importing {filetype} info")                     #   to be uploaded to Airtable
 
     content = []                                            # list of dictionaries, one dictionary per airtable row
+    if filetype == "Mack":
+        reg = re.compile(r'')
+    elif filetype == "Volvo":
+        reg = re.compile(r'')
+
+    pattern = reg.search(file)
 
     return content
     
@@ -78,20 +96,23 @@ def posttoairtable(content):                                # uploads the data t
     print("Post response: ",x.json())
     print("\n Post HTTP code:", x)
 
-testdata={
-    "records": [
-        {
-            "fields": {
-                "Name":"TULtest",
-                "Status":"Does Not Exist"
+if debug == True and sendtestdata == False:
+    testdata={
+        "records": [
+            {
+                "fields": {
+                    "Name":"TULtest",
+                    "Status":"Does Not Exist"
+                }
+            },
+            {
+                "fields": {
+                    "Name":"TULtest2",
+                    "Status":"Does Not Exist"
+                }
             }
-        },
-        {
-            "fields": {
-                "Name":"TULtest2",
-                "Status":"Does Not Exist"
-            }
-        }
-    ]
-}
-posttoairtable(testdata)
+        ]
+    }
+    posttoairtable(testdata)
+else:
+    checkfornewfiles()                      # main
