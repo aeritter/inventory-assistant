97c64d06a981f20afdfe64fafe1ffd57aebc5a66
aeritter <aritter@tamu.edu>
2020-02-11 16:16:12 -0600

    Now working

--- import-pictures.py
+++ import-pictures.py
@@ -1,4 +1,4 @@
-import os.path, csv, wget
+import os.path, csv, wget, time
 from multiprocessing.pool import ThreadPool
 
 imageURLfile = "C:\\imageURLs.csv"
@@ -9,18 +9,21 @@ with open(imageURLfile, 'r') as f:
     imageURLs = list(reader)
 
 with open(currentinventoryfile, 'r') as f:
-    reader = csv.reader(f)
-    currentinventory = set(reader)
+    currentinventory = set(line.strip() for line in f)
 
 listofimages = []
 def writeimages(url):
+    print("\n"+url)
+    time.sleep(.2)
     wget.download(url, "C:\\images\\")
 
 for x in imageURLs:                 # currently set to download to local PC, will eventually change to airtable upload
     if x[0] in currentinventory:
-        print("Downloading "+x[0])
-        listofimages += x[1]
+        listofimages.append(x[1])
         
 
-ThreadPool(8).imap_unordered(writeimages, listofimages)
+ThreadPool(12).imap_unordered(writeimages, listofimages)
+
+while True:                         # if this isn't here, the threads will be closed immediately after the above line creates them
+    time.sleep(30)
 
