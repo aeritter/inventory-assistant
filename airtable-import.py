1befe24cefe707e5fd5b552ad13e882b37cf3f6b
aeritter <aritter@tamu.edu>
2020-02-12 15:04:56 -0600

    Change regex and pdf/txt method
    Changed so that finding the correct info will be done through searching a list in Python rather than through many RegEx groups, making it easier to understand and make changes if the pdf formats change in the future.

--- airtable-import.py
+++ airtable-import.py
@@ -18,27 +18,30 @@ with open('C:\\airtabletest\\url.txt', 'r') as url:         #   location of .txt
 
 filefolder = 'C:\\airtabletest\\python-test\\'              # location of .pdf files
 pdftotextlocation = 'C:\\airtabletest\\pdftotext'           # location of pdftotext.exe (obtained from xpdfreader.com commandline tools)
+Mackregex = re.compile(r'^(.+?) {2,}(.*)\n')
+Volvoregex = re.compile(r'')
+
 
 def checkfornewfiles():
 #    try:
         for x in os.listdir(filefolder):
             if str(x)[-18:] != 'unknown format.txt' and str(x)[-3:] != 'txt': #   if filename doesn't contain 'unknown format.txt'
                                                                               #   and doesn't contain 'txt' at the end, then:
-                subprocess.run([pdftotextlocation, '-nopgbrk', filefolder+str(x)]) # convert pdf to text
+                subprocess.run([pdftotextlocation, '-nopgbrk', '-table','-margint', '70', '-marginb','40', filefolder+str(x)]) # convert pdf to text
                 filepath = filefolder+str(x)[:-4]           # create string of filepath to .txt file
                 filetype = "None"
                 print(filepath)
                 while os.path.exists(filepath+".txt") != True:
-                    time.sleep(.5)
+                    time.sleep(5)
                     print("Waiting for file creation")
 
                 with open(filepath+'.txt', 'r+') as c:
                     n = c.read()
                 line1 = n.split('\n', 1)[0]                 # first line of .txt file
-                line2 = n.split('\n', 2)[1]                 # second line of .txt file
-                if line1.startswith("Welcome to Volvo"):
+                # line2 = n.split('\n', 2)[1]                 # second line of .txt file
+                if "Selected & Major" in line1:
                     filetype = "Volvo"
-                elif line2.startswith("GSO:"):
+                elif "Model Year:" in line1:
                     filetype = "Mack"
                 else:
                     print("Unknown format")
@@ -52,12 +55,14 @@ def checkfornewfiles():
                             time.sleep(3)
                             print("Permission error. Trying {x} more time(s)") 
                             x -= 1
+                        except FileExistsError:
+                            print("File exists")
+                            break
 
                 if filetype != "None":
                     print(filetype)
-                    n = n.replace('\n', ' ')                # replace \n in file for easier RegEx processing (simplification)
                     if debug == True:
-                        writefile(n, filepath)            
+                        writefile(n, filepath, " (debug).txt")
                     dataimport(n, filetype)
                     os.remove(filepath+'.txt')               
 
@@ -66,22 +71,23 @@ def checkfornewfiles():
  #       print("something went wrong")
 
 
-def writefile(n, filepath):                                 # write file for debugging
-    a = open(filepath+' (debug).txt', 'w')
+def writefile(n, filepath, extension):                                 # write file for debugging
+    a = open(filepath+extension, 'w')
     a.write(n)
     a.close()
 
 def dataimport(file, filetype):                             #   takes the file and processes it to take out the relevant information
-    time.sleep(1)                                           #   according to which vendor it came from, then returns a list of dictionaries
+    time.sleep(.4)                                           #   according to which vendor it came from, then returns a list of dictionaries
     print(f"Importing {filetype} info")                     #   to be uploaded to Airtable
 
     content = []                                            # list of dictionaries, one dictionary per airtable row
     if filetype == "Mack":
-        reg = re.compile(r'')
+#        reg = re.compile(r'.*?Year:(\w*).*?MODEL\) (.*?)\n.*?ENGINE PACKAGE (.*?)\n.*?TRANSMISSION (.*?)\n.*?FRONT AXLE.*?AXLE.*?AXLE (.*?)\n.*?REAR AXLES - TANDEM (.*?)\n.*?REAR AXLE RATIO RATIO (\d\.\d\d).*?SUSPENSION - TANDEM (.*?)\n.*?DIFFERENTIAL (.*?)\n.*?WHEELBASE (.*?)\n.*?FUEL TANK - LH (.*?)\n.*?FIFTH WHEEL (.*?)\n.*?SLEEPER BOX (.*?)\n.*?DOOR OPENING OPTIONS (.*?)\n.*?MIRRORS - EXTERIOR (.*?)\n.*?REFRIGERATOR (.*?)\n.*?INVERTER - POWER (.*?)\n.*?TIRES BRAND/TYPE - FRONT (.*?)\n.*?WHEELS - FRONT (.*?)\n.*?TIRES BRAND/TYPE - REAR (.*?)\n.*?WHEELS - REAR (.*?)\n.*?PAINT COLOR - AREA A (.*?)\n.*?PRICE BOOK\nLEVEL:\n(.*?)\n',g,s,)
+        print()
     elif filetype == "Volvo":
-        reg = re.compile(r'')
+        print()
 
-    pattern = reg.search(file)
+    # pattern = reg.search(file)
 
     return content
     
